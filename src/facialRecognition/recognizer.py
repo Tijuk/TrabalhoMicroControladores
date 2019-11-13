f8e5cf1f9bd2130631eb2ee83d5c439cd6144ab3
jg-alvarez <57723984+jg-alvarez@users.noreply.github.com>
2019-11-13 15:53:59 -0300

    Update recognizer.py

--- src/facialRecognition/recognizer.py
+++ src/facialRecognition/recognizer.py
@@ -17,8 +17,6 @@ names = ['None', 'Marcelo', 'Paula', 'Ilza', 'Z', 'W']
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
 
 # Define min window size to be recognized as a face
 minW = 0.1*cam.get(3)
@@ -26,7 +24,6 @@ minH = 0.1*cam.get(4)
 
 def loop():
     ret, img =cam.read()
-    img = cv2.flip(img, -1) # Flip vertically
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
     faces = faceCascade.detectMultiScale( 
@@ -43,10 +40,10 @@ def loop():
         # Check if confidence is less them 100 ==> "0" is perfect match 
         if (confidence < 100):
             id = names[id]
-            confidence = "  {0}%".format(round(100 - confidence))
+            confidence = "  {0}%".format(round(confidence))
         else:
             id = "unknown"
-            confidence = "  {0}%".format(round(100 - confidence))
+            confidence = "  {0}%".format(round(confidence))
         
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
