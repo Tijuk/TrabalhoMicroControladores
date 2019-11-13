c6dfef9de5b092184fafad9b221983c6431517c8
jg-alvarez <57723984+jg-alvarez@users.noreply.github.com>
2019-11-13 15:51:48 -0300

    Update dataGatering.py

--- src/facialRecognition/dataGatering.py
+++ src/facialRecognition/dataGatering.py
@@ -2,10 +2,8 @@ import cv2
 import os
 
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video width
-cam.set(4, 480) # set video height
 
-face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_detector = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
 
 # For each person, enter one numeric face id
 face_id = input('\n enter user id end press <return> ==>  ')
@@ -16,9 +14,13 @@ count = 0
 
 while(True):
     ret, img = cam.read()
-    img = cv2.flip(img, -1) # flip video image vertically
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    faces = face_detector.detectMultiScale(
+        gray,     
+        scaleFactor=1.3,
+        minNeighbors=5,     
+        minSize=(20, 20)
+    )
 
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
