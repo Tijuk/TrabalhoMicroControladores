a0ea64f5267fbf0188b3ae3b4b5a8ca4902b1244
jg-alvarez <57723984+jg-alvarez@users.noreply.github.com>
2019-11-13 18:09:42 -0300

    Add files via upload

--- src/facialRecognition/dataGatering.py
+++ src/facialRecognition/dataGatering.py
@@ -1,5 +1,42 @@
 import cv2
 import os
+import numpy as np
+from PIL import Image
+
+# function to get the images and label data
+def getImagesAndLabels(path):
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+    faceSamples=[]
+    ids = []
+    for imagePath in imagePaths:
+        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        img_numpy = np.array(PIL_img,'uint8')
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+    return faceSamples,ids
+
+def trainer():
+    getImagesAndLabels(path)
+    print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+    faces,ids = getImagesAndLabels(path)
+    recognizer.train(faces, np.array(ids))
+
+    # Save the model into trainer/trainer.yml
+    recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+
+    # Print the numer of faces trained and end program
+    print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+    
+    
+# Path for face image database
+path = 'dataset'
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml');
+
 
 cam = cv2.VideoCapture(0)
 
@@ -41,3 +78,4 @@ while(True):
 print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
 cv2.destroyAllWindows()
+trainer()
