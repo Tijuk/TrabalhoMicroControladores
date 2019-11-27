c8cd734a5fa4949fa192577a2c88f8d36f40aa38
jg-alvarez <57723984+jg-alvarez@users.noreply.github.com>
2019-11-27 17:57:37 -0300

    Add files via upload

--- src/keyboard-login/loginByPassword.py
+++ src/keyboard-login/loginByPassword.py
@@ -1,5 +1,10 @@
 from pynput.keyboard import Key, Listener
 from threading import Timer
+from Adafruit_CharLCD import Adafruit_CharLCD
+import time
+
+#lcd = Adafruit_CharLCD(2, 3, 4, 5, 6, 7, 16, 2)
+lcd = Adafruit_CharLCD(7, 8, 25, 24, 23, 18, 20, 4)
 
 incorrectCounter = 0
 
@@ -13,12 +18,16 @@ def on_press(key):
     global passwordTyped
     try:
         passwordTyped += key.char
-        print('{0}'.format(passwordTyped))
+        lcd.clear()
+        printInitialMessage()
+        lcd.message('{0}'.format(passwordTyped))
     except AttributeError:
         if key == Key.enter:
             checkPassword()
         else:
-            print('Invalid character.')
+            lcd.clear()
+            printInitialMessage()
+            lcd.message('Invalid character.')
             
 def stopListener():
     global timer, listener
@@ -28,30 +37,39 @@ def stopListener():
 
 def checkPassword():
     global passwordTyped, incorrectCounter
+    
+    lcd.clear()
+    
     if passwordTyped == correctPassword:
-        print('Access granted')
+        lcd.message('Access granted')
+        time.sleep(1)
         incorrectCounter = 0
         printInitialMessage()
     else:
         incorrectCounter += 1
-        print('Access denied: {0} of 3'.format(incorrectCounter))
+        lcd.message('Access denied\n{0} of 3 chances'.format(incorrectCounter))
+        time.sleep(1)
         
         if (incorrectCounter < 3):
             printInitialMessage()
         else:
             incorrectCounter = 0
-            print('Access blocked. Wait 3 seconds and try again.')
+            lcd.clear()
+            lcd.message('Access blocked.\nWait 3 seconds\nand try again.')
             stopListener()
     
     passwordTyped = ''
     
 def printInitialMessage():
-    print('Type the password [hit Enter when ends]: ')
+    lcd.clear()
+    lcd.message('Type the password\nHit Enter when ends: ')
     
 def startListener():
     global timer, listener
     if (timer != None):
         timer.cancel()
+        
+    lcd.clear()
         
     printInitialMessage()
     
